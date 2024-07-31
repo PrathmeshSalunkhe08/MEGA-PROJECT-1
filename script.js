78402998038923fde3fca4e2d388f36b86020278
Prathmesh Salunkhe <prathameshsalunkhe102@gmail.com>
2024-08-01 03:53:33 +0530

    LODER COMPLETED

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-gsap.from('.line h1,h2',{
-    y:150,
-    opacity:0,
-    duration:0.5,
-    stagger:0.3,
-    delay:0.4
+var tl = gsap.timeline();
+
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+
+tl.from("#line1-part1, .line h2", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+      }
+    }, 33);
+  },
+});
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1,
 })
-let h5timer=document.querySelector('#line1-part1 h5');
-let grow=0;
-setInterval(() => {
 
-    if(grow<100){
-       
-        // console.log(grow);
-        h5timer.textContent = grow++;
+// Change font style after the initial animations
+tl.to(".line h2", {
+  fontFamily: "Silk Serif",
+  duration: 1,
+  delay: 2, // Adjust delay as needed
+});
 
-       
-    }
-    else{
-        // grow=100;
-        h5timer.textContent = grow;
-        // console.log(grow);
-    }
-   
-    
-}, 33);
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 2,
+});
 
-var tl=gsap.timeline();
-tl.to('#loader',{
-    opacity:0,
-    duration:0.3,
-    delay:4.1
+tl.from("#page1", {
+  delay: 0.5,
+  y: 1600,
+  opacity: 0,
+  duration: 0.5,
+});
 
-})
+tl.to("#loader", {
+  display: "none",
+});
